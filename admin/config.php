c7336a6179dcb60bba9b891e43f3dc98da8f5856
khoant <nguyenkhoabk@gmail.com>
2017-05-23 22:36:29 +0700

    Upload source code

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://gianglong.dev/admin/');
+define('HTTP_CATALOG', 'http://gianglong.dev/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://gianglong.dev/admin/');
+define('HTTPS_CATALOG', 'http://gianglong.dev/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/gianglong/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/gianglong/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/gianglong/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/gianglong/admin/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/gianglong/admin/view/template/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/gianglong/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/gianglong/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/gianglong/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/gianglong/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/gianglong/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/gianglong/system/storage/upload/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/gianglong/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'gianglong');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://gianglong.dev/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://gianglong.dev/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/gianglong/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/gianglong/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/gianglong/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/gianglong/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/gianglong/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/gianglong/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/gianglong/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/gianglong/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/gianglong/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/gianglong/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/gianglong/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'gianglong');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
